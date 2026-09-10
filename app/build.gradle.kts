5ff29b03fec64097d611c0ec58eaa68be1c67570
喵呜Miaowu <82693744+MiaoWuNYA@users.noreply.github.com>
2026-09-10 22:38:44 +0800

    chore: bump to 2.5.3fix1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 200
-        versionName = "2.5.3"
+        versionCode = 201
+        versionName = "2.5.3fix1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
