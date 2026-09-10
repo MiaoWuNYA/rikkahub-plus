46c08d417ef2698cab06c320a04a699beaa72320
喵呜Miaowu <82693744+MiaoWuNYA@users.noreply.github.com>
2026-09-10 13:07:52 +0800

    chore: bump to 2.5.2fix9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 195
-        versionName = "2.5.2fix8"
+        versionCode = 196
+        versionName = "2.5.2fix9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
